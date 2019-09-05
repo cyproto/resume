1182347a5e3787153362bbeb061ecc65d23c7dab
cyproto <kyashkarande@gmail.com>
2019-09-05 12:05:52 +0530

    Some updates and changes.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #Make a directory named "output"
 mkdir output
 
-#Compile and export the output pdf to already created directory.
+#Compile and export the output pdf to output directory.
 #fontspec package requires lualatex.
 lualatex --output-directory=output resume.tex 
